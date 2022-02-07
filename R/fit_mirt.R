37685cd475a7cc021105a0e45a94353625ad8ea0
Brian Norman Peña Calero <brianmsm@gmail.com>
2022-02-07 15:31:50 -0500

    Remove AICCs information

--- R/fit_mirt.R
+++ R/fit_mirt.R
@@ -10,7 +10,7 @@
 fit_mirt <- function(x) {
   fit_table <- tibble::tibble(
     AIC = mirt::extract.mirt(x, "AIC"),
-    AICc = mirt::extract.mirt(x, "AICc"),
+    # AICc = mirt::extract.mirt(x, "AICc"), # https://github.com/philchalmers/mirt/commit/1c6aeb8b43f21054b62c0b01001b2f3184514029
     BIC = mirt::extract.mirt(x, "BIC"),
     SABIC = mirt::extract.mirt(x, "SABIC"),
     "-2LL" = (mirt::extract.mirt(x, "logLik"))*-2
